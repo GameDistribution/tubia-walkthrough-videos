2ed135b206571a510130e03f348a4de0779769ff
Arthur Hulsman <a.hulsman@orangegames.com>
2018-07-13 09:41:32 +0200

    Stop tracking script file name loaded by publishers. We know enough now.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -105,15 +105,15 @@ class Tubia {
     init() {
         // Todo: Temporary tracking of wrong domains at publishers.
         /* eslint-disable */
-        if (typeof window['ga'] !== 'undefined' &&
-            document.currentScript) {
-            window['ga']('tubia.send', {
-                hitType: 'event',
-                eventCategory: 'LEGACY_URL_TRACKING',
-                eventAction: document.currentScript.src,
-                eventLabel: this.options.domain,
-            });
-        }
+        // if (typeof window['ga'] !== 'undefined' &&
+        //     document.currentScript) {
+        //     window['ga']('tubia.send', {
+        //         hitType: 'event',
+        //         eventCategory: 'LEGACY_URL_TRACKING',
+        //         eventAction: document.currentScript.src,
+        //         eventLabel: this.options.domain,
+        //     });
+        // }
         /* eslint-enable */
 
         const container = document.getElementById(this.options.container);
