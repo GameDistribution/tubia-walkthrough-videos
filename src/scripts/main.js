127d78d02d22c530f78c8b59464db41d8219e64d
Arthur Hulsman <a.hulsman@orangegames.com>
2018-10-12 12:56:21 +0200

    Changed the display ad slot name.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -445,7 +445,7 @@ class Tubia {
                     }
 
                     // window.googletag.defineSlot('1015413/TNL_NS-18062500055/TNL_T-18082776963_1', [728, 90], slotId)
-                    window.googletag.defineSlot('/21731147099/leaderboard_728x90_mid_page', [728, 90], slotId)
+                    window.googletag.defineSlot('/21731147099/Tubia_prestart_leaderboard', [728, 90], slotId)
                         .defineSizeMapping(mapping)
                         .setCollapseEmptyDiv(true, true)
                         .addService(window.googletag.pubads());
