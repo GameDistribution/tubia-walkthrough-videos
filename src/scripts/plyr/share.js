ffa53f6ccc08a32b688df593b883d6a413732cfd
Savaş YILMAZ <savasyilmz@gmail.com>
2019-04-11 09:53:31 +0300

    share test 6

--- src/scripts/plyr/share.js
+++ src/scripts/plyr/share.js
@@ -43,28 +43,25 @@ const share = {
 
         document.querySelector('.plyr--share-button').addEventListener('click', () => {
 
-            const linkParameters = {
-                publisherId: window.TUBIA_OPTIONS.publisherId,
-                title: window.TUBIA_OPTIONS.title,
-                gameId: window.TUBIA_OPTIONS.gameId,
-                colorMain: window.TUBIA_OPTIONS.colorMain,
-                colorAccent: window.TUBIA_OPTIONS.colorAccent,
-                langCode: window.TUBIA_OPTIONS.langCode,
-                keys: window.TUBIA_OPTIONS.keys,
-                url: window.location.href,
-                href: window.location.href,
-            };
-    
+            const params = utils.getUrlParams(document.location.href) || {};
+
+            const publisherId = typeof publisherIdLegacy !== 'undefined' && publisherIdLegacy !== '' ? publisherIdLegacy : '';
+            const gameId = typeof params.gameid !== 'undefined' && params.gameid !== '' ? params.gameid : '';
+            const title = typeof params.title !== 'undefined' && params.title !== '' ? params.title : '';
+            const colorMain = typeof params.colormain !== 'undefined' && params.colormain !== '' ? params.colormain : '';
+            const colorAccent = typeof params.coloraccent !== 'undefined' && params.coloraccent !== '' ? params.coloraccent : '';
+            const keys = utils.parseJson(params.keys);
+
             const shareLink = `https://player.tubia.com/index.html?
-                                &publisherid=${linkParameters.publisherId}
-                                &title=${linkParameters.title}
-                                &gameid=${linkParameters.gameId}
-                                &colormain=${linkParameters.colorMain}
-                                &coloraccent=${linkParameters.colorAccent}
-                                &langcode=${linkParameters.langCode}
-                                &keys=${linkParameters.keys}
-                                &url=${linkParameters.url}
-                                &href=${linkParameters.href}`;
+                                &publisherid=${publisherId}
+                                &title=${title}
+                                &gameid=${gameId}
+                                &colormain=${colorMain}
+                                &coloraccent=${colorAccent}
+                                &langcode=${langCode}
+                                &keys=${keys}
+                                &url=${document.location.href}
+                                &href=${document.location.href}`;
             
             const shareScreenContent = `
             <input type="text" class="plyr--share-fullscreen-input" value="${shareLink}"/>
@@ -91,7 +88,7 @@ const share = {
                 </a>
             </div>
             `;
-            
+
             shareScreen.innerHTML = shareScreenContent;
             plyrWrapper.appendChild(shareScreen);
         });
