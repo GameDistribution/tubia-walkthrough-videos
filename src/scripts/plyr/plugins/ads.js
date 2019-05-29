37d1242416366210ef1f230f3f4bcc64828c5472
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-05-29 14:27:16 +0300

    Add svg to toggle button

--- src/scripts/plyr/plugins/ads.js
+++ src/scripts/plyr/plugins/ads.js
@@ -262,7 +262,7 @@ class Ads {
 
         this.elements.toggleButtonContainer.appendChild(utils.createElement('span', {
             class: 'toggle-button', 
-        }, '-'));
+        }, '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 22h-24l12-20z"/></svg>'));
 
         this.elements.toggleButtonContainer.style.visibility = 'hidden';
         this.elements.toggleButtonContainer.addEventListener('click', this.toggleAd);
