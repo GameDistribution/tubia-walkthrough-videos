6b900f45be05147c17a0af9ddb8fe7d1da7082d7
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-08-06 16:26:43 +0300

    [TP-72] Call main.js minified or beautified version from a loader file depends on the location href

---
+++ src/scripts/plyr/.load.js
@@ -0,0 +1,8 @@
+const loadJs = document.querySelectorAll('[load-main-js]')[0];
+const js = document.createElement('script');
+const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || location.href.replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test';
+const fileExtension = debug ? '.js' : '.min.js';
+
+js.src = `./libs/gd/main${fileExtension}`;
+js.async= 'true';
+loadJs.parentNode.insertBefore(js, loadJs);
