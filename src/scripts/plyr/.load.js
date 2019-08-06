5fe5d4c66f7a7502fa9df4e3321528df84f9901a
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-08-06 16:58:24 +0300

    Use ancestorOrigins to check real domain

--- src/scripts/plyr/.load.js
+++ src/scripts/plyr/.load.js
@@ -1,6 +1,6 @@
 const loadJs = document.querySelectorAll('[load-main-js]')[0];
 const js = document.createElement('script');
-const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || location.href.replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test';
+const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || location.ancestorOrigins[0].replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test';
 const fileExtension = debug ? '.js' : '.min.js';
 
 js.src = `./libs/gd/main${fileExtension}`;
