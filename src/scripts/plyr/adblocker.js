30190fc46881e08938738e09a8c9b29fbbdfa0a7
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-08-06 11:36:41 +0300

    Move hb warning after adblocker message shown up

--- src/scripts/plyr/adblocker.js
+++ src/scripts/plyr/adblocker.js
@@ -44,7 +44,12 @@ const adblocker = {
 
             container.append(adblockWarning);
 
-            
+            setTimeout(() => {
+                if (adblockWarning.classList.contains('invisible')){
+                    adblockWarning.classList.remove('invisible');
+                }
+            }, 500);
+
             // Configure HoneyBadger tool.
             const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || location.href.replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test';
             const hbConfig = {
@@ -56,6 +61,7 @@ const adblocker = {
             // eslint-disable-next-line no-undef
             Honeybadger.configure(hbConfig);
             
+            // Notify HoneyBadger found an adblocker plugin.
             const hbObj = {
                 message: 'An adblocker plugin has been found. The player could not be started.',
                 name: 'AdBlocker',
@@ -67,12 +73,6 @@ const adblocker = {
             
             // eslint-disable-next-line no-undef
             Honeybadger.notify('error', hbObj);
-
-            setTimeout(() => {
-                if (adblockWarning.classList.contains('invisible')){
-                    adblockWarning.classList.remove('invisible');
-                }
-            }, 500);
         });
 
         return adsblocked;
