06f1bc8325b71ef011de05366186be685d2ff01b
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-08-07 10:18:54 +0300

    Revise loader template

--- src/scripts/plyr/.load.js
+++ src/scripts/plyr/.load.js
@@ -1,8 +1,19 @@
-const loadJs = document.querySelectorAll('[load-main-js]')[0];
-const js = document.createElement('script');
-const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || location.ancestorOrigins[0].replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test';
+// ==========================================================================
+// Load main script
+// ==========================================================================
+
+const locationAncestorOrigin = (location.ancestorOrigins.length > 0) ? location.ancestorOrigins[0].replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test' : false;
+const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || locationAncestorOrigin;
+
 const fileExtension = debug ? '.js' : '.min.js';
 
-js.src = `./libs/gd/main${fileExtension}`;
-js.async= 'true';
-loadJs.parentNode.insertBefore(js, loadJs);
+
+const js = document.createElement('script');
+
+js.src = `./libs/gd/main${fileExtension}`
+js.async = true;
+js.defer = true;
+
+const loader = document.getElementById('loader');
+loader.parentNode.insertBefore(js, loader);
+
