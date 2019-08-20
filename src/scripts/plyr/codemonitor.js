d7a743079995aa3c4c53cc7291bd76924708bb00
denizhacisalihoglu <denizhsalihoglu@gmail.com>
2019-08-20 10:34:39 +0300

    Add ngrok as a debugging domain

--- src/scripts/plyr/codemonitor.js
+++ src/scripts/plyr/codemonitor.js
@@ -12,7 +12,7 @@ const CodeMonitor = () => {
     if (!typeof location.ancestorOrigins === 'undefined') {
         locationAncestorOrigin = (location.ancestorOrigins.length > 0) ? location.ancestorOrigins[0].replace(/^(?:https?:\/\/)?/i, '').split('/')[0].split('.')[0] === 'test' : false;
     }
-    const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || locationAncestorOrigin;
+    const debug = (location.hostname === 'localhost' || location.hostname === '127.0.0.1') || locationAncestorOrigin || location.hostname.split('.')[1] === 'ngrok';
     const hbConfig = {
         apiKey: 'b4753ed6',
         environment: debug ? 'development' : 'production',
